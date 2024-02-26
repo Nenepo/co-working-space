9677262e64172e89ac9855261151b683003c7a62
Nafisa <Nafisafaruna@gmail.com>
2024-02-26 14:34:14 +0100

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,25 +9,37 @@ module.exports = {
         'white': '#FFFFFF',
         'grey': 'rgba(255, 255, 255, 0.50)',
         'textWhite': 'rgba(255, 255, 255, 0.80)',
-        'ash': ' #252626'
+        'ash': ' #252626',
+        'off-white': '#FFF8F4',
+        'header-text': ' #06060B',
+        'text': ' #0C0E12CC ',
+        'pink': '  #FFEFE7'
       },
      fontFamily: {
       'source' : ['Source Code Pro','monospace'],
+    
       'raleway': ['Raleway']
      },
 
      fontSize:{
       'xl': '1.25rem',
-      '3xl': '1.7rem',
+      '2xl': '28px',
+      '3xl': '1.8rem',
       '4xl': '2.7rem',
-      '5xl': '3.5rem',
+      '5xl': '3rem',
       '6xl': ['4rem',{
         lineHeight: '4.5rem'
       }]
      },
      borderRadius: {
-      'oval': '100%'
-     }
+      'oval': '100%',
+      'full': '50%'
+      
+     },
+    
+    gridTemplateRows: {
+      'auto' : 'repeat(6, 100px)'
+    }
   },
   plugins: [],
  
