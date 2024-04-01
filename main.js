2d65f915c509e98c0c1a1776a3bf2e2b36713ea1
Nafisa <Nafisafaruna@gmail.com>
2024-04-01 11:13:04 +0100

    completed

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 const currentYear = new Date().getFullYear();
 
 const date = document.getElementById('date')
 date.textContent = currentYear;
-})
+
 
 const tabsBox = document.querySelector(".tabs-box");
 const tabsBoxButtons = document.querySelectorAll(".tabs-box .tab");
@@ -52,3 +52,63 @@ tabsBox.addEventListener("mousemove", dragging);
 tabsBox.addEventListener("touchmove", dragging);
 document.addEventListener("mouseup", dragStop);
 document.addEventListener("touchend", dragStop);
+
+});
+
+// JavaScript
+document.addEventListener('DOMContentLoaded', async function () {
+  const toggle = document.getElementById('toggle');
+  const arrowUp = document.getElementById('arrowUp');
+  const arrowDown = document.getElementById('arrowDown');
+  const locationName = document.getElementById('locationName');
+
+  // Fetch location name from API
+  // async function fetchLocationName() {
+  //     try {
+  //         const response = await fetch('your_api_endpoint_here');
+  //         const data = await response.json();
+  //         locationName.textContent = data.location; 
+  //         // Assuming the API returns an object with a 'location' property
+  //     } catch (error) {
+  //         console.error('Error fetching location name:', error);
+  //         locationName.textContent = 'Error loading location';
+  //     }
+  // }
+
+  toggle.addEventListener('click', function () {
+     
+      arrowUp.classList.toggle('hidden');
+      arrowDown.classList.toggle('hidden');
+  });
+
+  // Fetch location name when the page loads
+  // await fetchLocationName();
+
+
+
+  // Trending Product Slider-start
+var TrendingSlider = new Swiper('.swiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// Trending Product Slider-end
+
+
+});
